0284d54eafb7bb3c87876e34cd8f2e79743a8fdb
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-05-09 15:54:26 -0400

    scripts
    Translated a vanilla js to a jquery script.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function newItem(){   
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === ''){
+        alert("Please type your task!")
+    }else{
+        $('#list').append(li);
+    }
+
+function crossOut(){
+    li.toggleClass("strike");
+}
+li.on("dblclick", function crossOut(){
+    li.toggleClass("strike");
+});
+
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+    crossOutButton.on("click", deleteListItem);
+
+function deleteListItem(){
+    li.addClass('delete')
+}
+$('#list').sortable();
+}
